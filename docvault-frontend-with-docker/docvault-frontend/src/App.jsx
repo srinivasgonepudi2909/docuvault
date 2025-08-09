36cb0858346ea8765ae66267713ea64bd8717b0c
srinivas <srigonepudi@gmail.com>
2025-08-09 18:14:28 +0530

    updated

---
+++ docvault-frontend-with-docker/docvault-frontend/src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="app-shell">
+      <div className="app-card">
+        <h1>DocVault</h1>
+        <p>Securely store Images, PDFs, and Documents.</p>
+      </div>
+    </div>
+  );
+}
